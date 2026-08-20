341b9e4dfc23471a83a1c40b8dd8b9252a669f59
root <root@inst-builder-debian-12-build-build-23nv3.local>
2026-08-20 11:21:54 +0000

    download tree

--- job/gedi_tree_ratio/__main__.py
+++ job/gedi_tree_ratio/__main__.py
@@ -28,6 +28,45 @@ test_df = gpd.read_parquet(TEST_PARQUET)
 
 tile_ids = grids_df["tile_id"].unique()
 
+year = 2020
+
+data_id = ""
+if year == 2021:
+    data_id = 15032553
+elif year == 2020:
+    data_id = 15032488
+elif year == 2019:
+    data_id = 15032448
+elif year == 2018:
+    data_id = 15032393
+elif year == 2017:
+    data_id = 15032307
+
+paths = [
+    f"https://zenodo.org/records/{data_id}/files/Predicted_tree_coverage_ratio_from_GEDI_Sentinel_based_UNET_model_{year}_batch_{no}.tif?download=1"
+    for no in range(14)
+]
+
+
+def download(path):
+    folder = TemporaryDirectory(delete=False)
+    o = f"{folder.name}/output.tif"
+    check_call(f"curl {path} -X GET -o {o}", shell=True)
+    return o
+
+
+logger.info("Download all data")
+
+with ThreadPoolExecutor(8) as executor:
+    jobs = [executor.submit(download, path) for path in paths]
+    results = [job.result() for job in jobs]
+
+logger.info("Create VRT")
+
+folder = TemporaryDirectory(delete=False)
+vrt = f"{folder.name}/tree.vrt"
+check_call(f"""gdal raster mosaic -f VRT {" ".join(results)} {vrt}""", shell=True)
+
 
 def load_tree(index):
     grid = grids_df[index : index + 1]
@@ -37,50 +76,23 @@ def load_tree(index):
 
     xmin, ymin, xmax, ymax = tuple(grid.total_bounds)
 
-    train_bbox = train_df.cx[xmin:xmax, ymin:ymax]
-    test_bbox = test_df.cx[xmin:xmax, ymin:ymax]
+    name = f"tile_{tile_id}"
 
-    years = list(set([*train_bbox["year"].unique(), *test_bbox["year"].unique()]))
+    with TemporaryDirectory() as folder:
+        canopy = f"{folder}/tree.tif"
+        check_call(
+            f"""gdal raster pipeline \
+                ! read {vrt} \
+                ! reproject -d EPSG:4326 --bbox-crs=EPSG:4326 --bbox={xmin},{ymin},{xmax},{ymax} \
+                ! write -f COG --co="COMPRESS=ZSTD" {canopy} \
+          """,
+            shell=True,
+        )
 
-    for year in years:
-        name = f"tile_{tile_id}_{year}"
-
-        data_id = ""
-        if year == 2021:
-            data_id = 15032553
-        elif year == 2020:
-            data_id = 15032488
-        elif year == 2019:
-            data_id = 15032448
-        elif year == 2018:
-            data_id = 15032393
-        elif year == 2017:
-            data_id = 15032307
-
-        paths = [
-            f"/vsicurl/https://zenodo.org/records/{data_id}/files/Predicted_tree_coverage_ratio_from_GEDI_Sentinel_based_UNET_model_{year}_batch_{no}.tif?download=1"
-            for no in range(14)
-        ]
-
-        with TemporaryDirectory() as folder:
-            text = f"{folder}/tree.txt"
-            with open(text, "w") as file:
-                file.write("\n".join(paths))
-
-            canopy = f"{folder}/tree.tif"
-            check_call(
-                f"""gdal raster pipeline \
-                    ! mosaic --resolution=average @{text} \
-                    ! reproject -d EPSG:4326 --bbox-crs=EPSG:4326 --bbox={xmin},{ymin},{xmax},{ymax} \
-                    ! write -f COG --co="COMPRESS=ZSTD" {canopy} \
-              """,
-                shell=True,
-            )
-
-            check_call(
-                f"gcloud storage cp {canopy} gs://gee-ramiqcom-s4g-bucket/opengeohub_summerschool_2026/gedi_tree_ratio/{name}.tif",
-                shell=True,
-            )
+        check_call(
+            f"gcloud storage cp {canopy} gs://gee-ramiqcom-s4g-bucket/opengeohub_summerschool_2026/gedi_tree_ratio/{name}.tif",
+            shell=True,
+        )
 
 
 with ThreadPoolExecutor(8) as executor:
