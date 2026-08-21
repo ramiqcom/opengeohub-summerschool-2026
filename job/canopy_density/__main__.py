2344848d66c4742359161441621af805a4fe06e4
Ramadhan <ramiqcom@gmail.com>
2026-08-21 10:23:55 +0300

    cdse success

--- job/canopy_density/__main__.py
+++ job/canopy_density/__main__.py
@@ -1,16 +1,21 @@
+import os
 from concurrent.futures import ThreadPoolExecutor
 from subprocess import check_call
 from tempfile import TemporaryDirectory
 
 import geopandas as gpd
-import pandas as pd
-from shapely import from_wkt
+from pystac_client import Client
 
 from ..utils import logger
 
 OUTPUT_VOLUME = "/usr/src/app/output"
 
-COL = "https://s3.waw3-1.cloudferro.com/swift/v1/CatalogueCSV/landcover_landuse/tree_cover_density/clms_vlcc_tree-cover-density_europe_10m_yearly_v1/clms_vlcc_tree-cover-density_europe_10m_yearly_v1_cog.csv"
+os.environ["AWS_S3_ENDPOINT"] = "eodata.dataspace.copernicus.eu"
+os.environ["AWS_VIRTUAL_HOSTING"] = "FALSE"
+os.environ["AWS_NO_SIGN_REQUEST"] = "NO"
+
+STAC = "https://stac.dataspace.copernicus.eu/v1"
+COL_NAME = "clms_vlcc_tree-cover-density_europe_10m_yearly_v1"
 ROI = "https://storage.googleapis.com/gee-ramiqcom-s4g-bucket/opengeohub_summerschool_2026/roi/tiles_v2.geojson"
 
 CLOUD_PREFIX = "gs://gee-ramiqcom-s4g-bucket/opengeohub_summerschool_2026"
@@ -20,11 +25,7 @@ TRAIN_PARQUET = f"{OUTPUT_VOLUME}/train_only_biomass.parquet"
 TEST_PARQUET = f"{OUTPUT_VOLUME}/test_only_biomass.parquet"
 GRIDS = "https://storage.googleapis.com/gee-ramiqcom-s4g-bucket/opengeohub_summerschool_2026/roi/tiles_v2.geojson"
 
-col_df = gpd.read_file(COL, delimiter=";")
-col_df = gpd.GeoDataFrame(col_df, geometry=[from_wkt(pol) for pol in col_df["bbox"]])
-col_df["content_date_start"] = pd.to_datetime(col_df["content_date_start"])
-col_df["content_date_end"] = pd.to_datetime(col_df["content_date_end"])
-columns = col_df.columns
+catalog = Client.open(STAC)
 
 logger.info("Load grids")
 grids_df = gpd.read_file(GRIDS)
@@ -46,8 +47,6 @@ def load_density(index):
 
     xmin, ymin, xmax, ymax = tuple(grid.total_bounds)
 
-    col_filter = col_df.cx[xmin:xmax, ymin:ymax]
-
     train_bbox = train_df.cx[xmin:xmax, ymin:ymax]
     test_bbox = test_df.cx[xmin:xmax, ymin:ymax]
 
@@ -59,15 +58,21 @@ def load_density(index):
         date_start = f"{year}-01-01"
         date_end = f"{year}-12-31"
 
-        col_filter_year = col_filter[
-            col_filter["content_date_start"].between(date_start, date_end)
+        col_filter_year = [
+            data
+            for data in catalog.search(
+                collections=[COL_NAME],
+                datetime=(date_start, date_end),
+                bbox=(xmin, ymin, xmax, ymax),
+            ).items_as_dicts()
         ]
 
         if len(col_filter_year) > 0:
             paths = [
-                f""" "{path.replace("s3://", "/vsis3/")}" """
-                for path in col_filter_year["s3_path"]
+                f"{feat['assets']['data']['href'].replace('s3://', '/vsis3/')}"
+                for feat in col_filter_year
             ]
+            logger.info(paths)
 
             with TemporaryDirectory() as folder:
                 canopy = f"{folder}/canopy.tif"
@@ -95,9 +100,9 @@ def load_density(index):
             logger.info(f"No data in {name}")
 
 
-with ThreadPoolExecutor(8) as executor:
+with ThreadPoolExecutor(32) as executor:
     jobs = []
-    for index in range(1):
+    for index in range(len(tile_ids)):
         jobs.append(executor.submit(load_density, index))
 
     for job in jobs:
